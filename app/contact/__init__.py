4f0fcb0fc17ba6ad10beef062382d8fffa6fd835
Tammy Davies <daviests4@cardiff.ac.uk>
2025-01-04 21:20:19 +0000

    feat: added priority sorting to blogs and updated 'about' content

--- app/contact/__init__.py
+++ app/contact/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from .routes import contact
 
 # Define the blueprint
-contact_bp = Blueprint('contact', __name__, template_folder='templates')
+contact_bp = Blueprint("contact", __name__, template_folder="templates")
 
 # Add the route
 contact_bp.add_url_rule("/", view_func=contact, methods=["GET", "POST"])
