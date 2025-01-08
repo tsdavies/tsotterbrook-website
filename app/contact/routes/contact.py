ed93fd872c50ce4fd10faeb238665d85ad4b7d0b
Tammy Davies <daviests4@cardiff.ac.uk>
2025-01-08 14:59:40 +0000

    chore: Ensure flask_wtf forms used everywhere

--- app/contact/routes/contact.py
+++ app/contact/routes/contact.py
@@ -1,26 +1,21 @@
 import os
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash
+from app.contact.forms.contact_form import ContactForm
 from flask_mail import Message
 from app.extensions import mail
 
 
 def contact():
-    if request.method == "POST":
-        name = request.form.get("name")
-        email = request.form.get("email")
-        message = request.form.get("message")
-
+    form = ContactForm()
+    if form.validate_on_submit():
         msg = Message(
-            subject=f"New Contact Form Submission from {name}",
-            sender=os.environ.get("MAIL_USERNAME"),  # Sender email from environment
-            recipients=[os.environ.get("MAIL_USERNAME")],  # Send to yourself
-            body=f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}",
+            subject=f"New Contact Form Submission from {form.name.data}",
+            sender=form.email.data,
+            recipients=[os.environ.get("MAIL_USERNAME")],
+            body=form.message.data
         )
-        try:
-            mail.send(msg)
-            flash(f"Thank you, {name}. Your message has been sent!", "success")
-        except Exception as e:
-            flash(f"An error occurred: {str(e)}", "danger")
-            return redirect(url_for("contact.contact"))
+        mail.send(msg)
+        flash("Your message has been sent successfully.", "success")
+        return redirect(url_for("contact"))
 
-    return render_template("contact.html", title="Contact Me")
+    return render_template("contact.html", form=form)
