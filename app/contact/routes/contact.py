90d9f4a8af102bd587600da44410c887b467fe31
Tammy Davies <daviests4@cardiff.ac.uk>
2025-01-08 17:29:27 +0000

    fix: enhance contact form security with input sanitisation using bleach

--- app/contact/routes/contact.py
+++ app/contact/routes/contact.py
@@ -1,21 +1,28 @@
 import os
+
+import bleach
 from flask import render_template, redirect, url_for, flash
-from app.contact.forms.contact_form import ContactForm
 from flask_mail import Message
+
+from app.contact.forms.contact_form import ContactForm
 from app.extensions import mail
 
 
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
+        # Sanitize inputs
+        sanitized_name = bleach.clean(form.name.data)
+        sanitized_email = bleach.clean(form.email.data)
+        sanitized_message = bleach.clean(form.message.data)
+
         msg = Message(
-            subject=f"New Contact Form Submission from {form.name.data}",
-            sender=form.email.data,
+            subject=f"New Contact Form Submission from {sanitized_name}",
+            sender=sanitized_email,
             recipients=[os.environ.get("MAIL_USERNAME")],
-            body=form.message.data
+            body=sanitized_message,
         )
         mail.send(msg)
         flash("Your message has been sent successfully.", "success")
-        return redirect(url_for("contact"))
-
+        return redirect(url_for("contact.contact"))
     return render_template("contact.html", form=form)
